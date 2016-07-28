13fc884c222a6dd2cefbf1f909d392ac254ee096
kevinsoo <kevin.soo@pitt.edu>
2016-07-28 14:41:27 -0400

    basic scraping

--- kevin_scrapeData.R
+++ kevin_scrapeData.R
@@ -7,17 +7,17 @@ theme_set(theme_bw())
 
 # this code scrapes the table for standard stats (11 pages)
 for (i in 1:11) {
-    fox <- paste("http://www.foxsports.com/soccer/stats?competition=1&season=20150&category=CONTROL&pos=0&team=0&isOpp=0&splitType=0&sort=4&sortOrder=0&page=", i, sep="")
+    fox <- paste("http://www.foxsports.com/soccer/stats?competition=1&season=20150&category=STANDARD&pos=0&team=0&isOpp=0&splitType=0&sort=3&sortOrder=0&page=", i, sep="")
     fox.table <- readHTMLTable(fox, header=T, which=1,stringsAsFactors=F)
     if (i==1) {
-        control.df <- fox.table
+        standard.df <- fox.table
     }
     else {
-        control.df <- rbind(control.df, fox.table)
+        standard.df <- rbind(standard.df, fox.table)
     }
 }
 
-head(control.df)
+head(standard.df)
 
 # this code scrapes the table for control stats (11 pages)
 for (i in 1:11) {
